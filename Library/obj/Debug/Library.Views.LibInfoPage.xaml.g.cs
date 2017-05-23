fe07ae937870e7572293ad942be1bd953773a941
fuyi1215 <fuyi1215@msn.com>
2017-05-23 10:42:07 -0700

    5232017

--- Library/obj/Debug/Library.Views.LibInfoPage.xaml.g.cs
+++ Library/obj/Debug/Library.Views.LibInfoPage.xaml.g.cs
@@ -11,7 +11,7 @@
 namespace Library {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("/Users/frank/Downloads/Butler Public Library copy 2/Library/Views/LibInfoPage.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("/Users/frank/Downloads/library/Library/Views/LibInfoPage.xaml")]
     public partial class LibInfoPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
