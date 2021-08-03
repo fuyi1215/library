69c4d77b6038e190fc605a7f9a2ecfd4aac80c94
Frank Fu <fuyi1215@msn.com>
2021-08-03 08:35:26 -0700

    Update NewsApiUrls.cs

--- Library/API/NewsApiUrls.cs
+++ Library/API/NewsApiUrls.cs
@@ -7,7 +7,7 @@ namespace Library.API
 
 		
 		public static readonly string calendar =
-            string.Format("https://www.googleapis.com/calendar/v3/calendars/greentownchildrenslib%40gmail.com/events?orderBy=startTime&singleEvents=true&timeMin={0}-{1}-{2}T00%3A00%3A00Z&fields=items&key=AIzaSyD57lAo8C1Ztr5ZgcHVsuyw1rXFyD1scyo"
+            string.Format("https://www.googleapis.com/calendar/v3/calendars/greentownchildrenslib%40gmail.com/events?orderBy=startTime&singleEvents=true&timeMin={0}-{1}-{2}T00%3A00%3A00Z&fields=items&key="
 			              , DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
 		//string.Format("https://www.googleapis.com/calendar/v3/calendars/becky%40butlerpubliclibrary.net/events?key=AIzaSyDJ8A1_7hSs-dNLGRehiDt_cOF1D_8_0YE", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
 		
